1159d69df723cb575fdbc34c14fcc3bf58055cb7
Archil Zivzivadze <a.zivzivadze@betsolutions.com>
2021-12-26 23:37:26 +0400

    class intro

--- OOP/Student.py
+++ OOP/Student.py
@@ -1,7 +1,9 @@
 class Student:
     def __init__(self, name, age, is_on_probation):
-        self.name = name
-        self.age = age
-        self.is_on_probation = is_on_probation
+        self.__name = name
+        self.__age = age
+        self.__is_on_probation = is_on_probation
 
-    
+    def getName(self):
+        return self.__name
+
---
+++ OOP/app.py
@@ -0,0 +1,5 @@
+from Student import Student
+
+student1 = Student("zulu", 31, True)
+
+print(student1.getName())
